8284490af6123f7543043f9d7f7b42befb18c91e
Roman Gritchenko <gritroma@yandex.ru>
2020-04-30 14:11:03 +0300

    Revert "изменила дизайн, немного лишнего в файлах поудаляла, настроила навигацию, когда выбираешь категорию там сверху её название появляется, как-то так"
    This reverts commit c3adfc49

--- app/src/main/java/com/example/mysecondproject/SettingsActivity.kt
+++ app/src/main/java/com/example/mysecondproject/SettingsActivity.kt
@@ -1,22 +1,16 @@
 package com.example.mysecondproject
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import kotlinx.android.synthetic.main.activity_settings.*
 
-class SettingsActivity : AppCompatActivity() {
+class SettingsActivity : BaseActivity(3) {
     private val TAG = "SettingsActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        setupBottomNavigation()
         Log.d(TAG, "onCreate")
-
-        back.setOnClickListener {
-            val intent = Intent(this, MoneyActivity::class.java)
-            startActivity(intent)
-        }
     }
 }
